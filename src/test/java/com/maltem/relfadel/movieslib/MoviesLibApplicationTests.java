f3eb8b22c5f6e6ce1897efe34a92a4f1fb5fdb05
Radouane EL FADEL <r.elfadel@cygnuscore.com>
2019-11-07 14:11:47 +0100

    :rocket:(build) Init project Closes #1

---
+++ src/test/java/com/maltem/relfadel/movieslib/MoviesLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maltem.relfadel.movieslib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

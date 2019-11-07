0734bb60be68bd8e11c7741611bb227b57c286a8
Radouane EL FADEL <r.elfadel@cygnuscore.com>
2019-11-07 15:03:06 +0100

    :rocket:(build) Create white app (Angular) Closes #2

---
+++ src/main/resources/movies-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'movies-app';
+}

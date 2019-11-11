14b795d82de0ad0823ef9f323634924a567ba803
Radouane EL FADEL <r.elfadel@cygnuscore.com>
2019-11-11 04:00:34 +0100

    refactor!

--- src/main/java/com/maltem/relfadel/movieslib/controllers/MoviesController.java
+++ src/main/java/com/maltem/relfadel/movieslib/controllers/MoviesController.java
@@ -1,10 +1,28 @@
 package com.maltem.relfadel.movieslib.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.maltem.relfadel.movieslib.dto.MovieDto;
+import com.maltem.relfadel.movieslib.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/movies")
 public class MoviesController {
+
+    @Autowired
+    MovieService movieService;
+
+    @GetMapping
+    public Iterable<MovieDto> all() {
+        return movieService.findAll();
+    }
+
+    @GetMapping(value = "/{uuid}")
+    public HttpEntity<MovieDto> findOne(@PathVariable String uuid) {
+        return new ResponseEntity<>(this.movieService.findOne(uuid), HttpStatus.OK);
+    }
 
 }
